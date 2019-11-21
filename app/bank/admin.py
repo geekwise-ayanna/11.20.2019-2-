777c3eab3a235d29a01c259e3a93a8bafbd03736
Ayanna Bowers-Shaw <geekwise.ayanna.bs@gmail.com>
2019-11-21 21:35:41 +0000

    fixed issue where other folders/files inside bank were not being recognized by adding __init__.py to each folder

--- app/bank/admin.py
+++ app/bank/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from bank.account.models import Account
+from bank.customer.models import Customer
+from bank.product.models import Product
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Product)
+admin.site.register(Customer)
